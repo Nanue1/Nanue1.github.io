0b08928031eca3b096c4c5f196d021f645ac1d4c
Nanue1 <manue1@foxmail.com>
2017-06-02 19:16:59 +0800

    1496402219

--- auto_deply_gtd_push.py
+++ auto_deply_gtd_push.py
@@ -2,16 +2,21 @@ import os
 import time
 arg_path = '/home/manue1/github/note/note-gtd.html'
 target_path = '/home/manue1/github/blog/source/about/'
+post_path = '/home/manue1/github/blog/source/_post/'
 if os.path.exists(arg_path):
     cmd = 'mv %s %s -f' % (arg_path,target_path)
     s = os.system(cmd)
-    print "copy status: %s" % str(s)
-    hexo = 'hexo g && hexo d' 
-    os.system(hexo)
+    print "copy not-gtd.html  status: %s" % str(s)
 else:
     print "not-gtd.html not exist!"
-    hexo = 'hexo g && hexo d' 
-    os.system(hexo)
+
+html = 'mv *.html %s -f' % post_path
+h = os.system(html)
+print "copy html status: %s" % str(h)
+
+
+hexo = 'hexo g && hexo d' 
+os.system(hexo)
 
 int_t = int(time.time())
 cmd = "git add . && git commit -m '%d' && git push " % int_t
